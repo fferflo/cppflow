488dfb2ddeebd4fa8baf287128392f2440a187c3
Florian Fervers <florian.fervers@gmail.com>
2021-03-19 15:14:15 +0100

    Add modern cmake

--- examples/efficientnet/create_model.py
+++ examples/efficientnet/create_model.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
+import os
 
 model = tf.keras.applications.EfficientNetB0()
 
 # Export the model to a SavedModel
-model.save('model', save_format='tf')
+model.save(os.path.join(os.path.dirname(os.path.realpath(__file__)), "model"), save_format='tf')
 
-
+print("Output shape: " + str(model.layers[-1].output.shape))
